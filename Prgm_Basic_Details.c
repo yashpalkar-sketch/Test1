eca97580a4f465f61e34b137ef634026efc746cf
yashpalkar-sketch <yashpalkar@eng.rizvi.edu.in>
2025-10-16 23:31:15 +0530

    Add files via upload

---
+++ Prgm_Basic_Details.c
@@ -0,0 +1,19 @@
+/*
+Name - Yash palkar
+Age- 18
+Class- F.E.
+DIV- D (Mechanical)
+UIN - 251M038
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Name- Yash Palkar\n");
+    printf("Age- 18\n");
+    printf("Class- F.E.\n");
+    printf("DIV- D (Mechanical)\n");
+    printf("UIN- 251M038\n");
+
+    return 0;
+}
